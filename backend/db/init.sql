f6ababa58c217c7fb3dcbedcdaf2a26f95178acf
ryuu000 <99164869+ryuu000@users.noreply.github.com>
2025-03-28 02:17:29 +0530

    Update init.sql

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,1 +1,14 @@
+CREATE TABLE profiles (
+    address TEXT PRIMARY KEY,
+    name TEXT,
+    bio TEXT,
+    profilePicture TEXT,
+    isWellnessProfessional BOOLEAN
+);
 
+CREATE TABLE votes (
+    voter TEXT,
+    wellnessProfessional TEXT,
+    timestamp TIMESTAMP,
+    PRIMARY KEY (voter, wellnessProfessional)
+);
